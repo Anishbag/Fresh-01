5991836216af212a5fff762079446a0045c50aa4
Anish Bag <anishbag18@gmail.com>
2026-07-31 13:19:56 +0530

    mailnew

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,8 +1,24 @@
+// import nodemailer from "nodemailer";
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export default transporter;
+
+
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
