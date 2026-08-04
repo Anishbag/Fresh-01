d9641277c52016acc50c99378de858096f4c1460
Anish Bag <anishbag18@gmail.com>
2026-08-04 10:28:07 +0530

    auto checkout

--- src/cron/attendanceCron.js
+++ src/cron/attendanceCron.js
@@ -1,39 +1,87 @@
+// import cron from "node-cron";
+// import Attendance from "../models/Attendance.js";
+
+// // Every day at 11:59 PM
+// cron.schedule("59 23 * * *", async () => {
+//   try {
+//     console.log("Running Auto Checkout...");
+
+//     const today = new Date();
+//     today.setHours(0, 0, 0, 0);
+
+//     const checkOutTime = new Date();
+//     checkOutTime.setHours(23, 59, 0, 0);
+
+//     const attendanceList = await Attendance.find({
+//       date: today,
+//       checkOut: null,
+//     });
+
+//     for (const attendance of attendanceList) {
+//       attendance.checkOut = checkOutTime;
+
+//       const diff =
+//         (attendance.checkOut - attendance.checkIn) /
+//         (1000 * 60 * 60);
+
+//       attendance.workingHours = Number(diff.toFixed(2));
+
+//       await attendance.save();
+//     }
+
+//     console.log(
+//       `${attendanceList.length} employee auto checked out`
+//     );
+
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+
+
 import cron from "node-cron";
 import Attendance from "../models/Attendance.js";
 
-// Every day at 11:59 PM
-cron.schedule("59 23 * * *", async () => {
-  try {
-    console.log("Running Auto Checkout...");
+// cron.schedule("* * * * *", async () => {
 
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
+  cron.schedule("59 23 * * *", async () => {
 
-    const checkOutTime = new Date();
-    checkOutTime.setHours(23, 59, 0, 0);
+    console.log("Auto Checkout Running...");
+
+    const start = new Date();
+    start.setHours(0,0,0,0);
+
+    const end = new Date();
+    end.setHours(23,59,59,999);
+
+    const autoCheckout = new Date();
 
     const attendanceList = await Attendance.find({
-      date: today,
-      checkOut: null,
+
+        date:{
+            $gte:start,
+            $lte:end
+        },
+
+        checkOut:null
+
     });
 
-    for (const attendance of attendanceList) {
-      attendance.checkOut = checkOutTime;
+    console.log(attendanceList.length);
 
-      const diff =
-        (attendance.checkOut - attendance.checkIn) /
-        (1000 * 60 * 60);
+    for(const attendance of attendanceList){
 
-      attendance.workingHours = Number(diff.toFixed(2));
+        attendance.checkOut = autoCheckout;
 
-      await attendance.save();
+        const hours =
+        (attendance.checkOut-attendance.checkIn)/(1000*60*60);
+
+        attendance.workingHours =
+        Number(hours.toFixed(2));
+
+        await attendance.save();
+
     }
 
-    console.log(
-      `${attendanceList.length} employee auto checked out`
-    );
-
-  } catch (err) {
-    console.log(err);
-  }
 });
