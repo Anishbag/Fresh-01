5eee11bb86fda2e1dd256777945b6aeb4eeb1884
Anish Bag <anishbag18@gmail.com>
2026-08-05 17:41:04 +0530

    checkIn and chackout  change2

--- src/controllers/employee/leaveController.js
+++ src/controllers/employee/leaveController.js
@@ -1,13 +1,51 @@
 import Employee from "../../models/Employee.js";
 import Leave from "../../models/Leave.js";
+import Attendance from "../../models/Attendance.js";
+
+// export const applyLeave = async (req, res) => {
+//   try {
+//     // const employee = await Employee.findOne({
+//     //   userId: req.user._id,
+//     //   isDeleted: false,
+//     // });
+
+//     const employee = await Employee.findOne({
+//       userId: req.user._id,
+//       status: "Active",
+//     });
+
+//     if (!employee) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "Employee not found",
+//       });
+//     }
+
+//     const { leaveType, fromDate, toDate, reason } = req.body;
+
+//     const leave = await Leave.create({
+//       employee: employee._id,
+//       leaveType,
+//       fromDate,
+//       toDate,
+//       reason,
+//     });
+
+//     res.status(201).json({
+//       success: true,
+//       message: "Leave applied successfully",
+//       leave,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
 
 export const applyLeave = async (req, res) => {
   try {
-    // const employee = await Employee.findOne({
-    //   userId: req.user._id,
-    //   isDeleted: false,
-    // });
-
     const employee = await Employee.findOne({
       userId: req.user._id,
       status: "Active",
@@ -22,6 +60,48 @@ export const applyLeave = async (req, res) => {
 
     const { leaveType, fromDate, toDate, reason } = req.body;
 
+    //  ager date to porer date kora jabe nah
+    if (new Date(fromDate) > new Date(toDate)) {
+      return res.status(400).json({
+        success: false,
+        message: "From date cannot be greater than To date.",
+      });
+    }
+
+    // ✅ pending ba approv thakle newa jabe nah
+    const existingLeave = await Leave.findOne({
+      employee: employee._id,
+      status: { $in: ["Pending", "Approved"] },
+      fromDate: { $lte: new Date(toDate) },
+      toDate: { $gte: new Date(fromDate) },
+    });
+
+    if (existingLeave) {
+      return res.status(400).json({
+        success: false,
+        message: "You already have a leave request for the selected dates.",
+      });
+    }
+
+    const attendance = await Attendance.findOne({
+  employee: employee._id,
+  // status: "Present",
+  date: {
+    $gte: new Date(fromDate),
+    $lte: new Date(toDate),
+  },
+});
+
+if (attendance) {
+  return res.status(400).json({
+    success: false,
+    message: "Attendance already exists for the selected date.",
+  });
+}
+
+
+
+
     const leave = await Leave.create({
       employee: employee._id,
       leaveType,
@@ -35,6 +115,7 @@ export const applyLeave = async (req, res) => {
       message: "Leave applied successfully",
       leave,
     });
+
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -42,6 +123,10 @@ export const applyLeave = async (req, res) => {
     });
   }
 };
+
+
+
+
 
 export const myLeaveHistory = async (req, res) => {
   try {
